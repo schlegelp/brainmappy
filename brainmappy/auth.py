0237933537d44120eb8e2075b6e9d8e96ef719e5
Philipp Schlegel <observing@web.de>
2025-02-25 16:13:52 +0000

    fix authentication

--- brainmappy/auth.py
+++ brainmappy/auth.py
@@ -12,31 +12,32 @@
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
 
-""" This module handles Google authentication """
+"""This module handles Google authentication"""
 
 import os
 import sys
 import pickle
+import json
 
-from google_auth_oauthlib.flow import InstalledAppFlow
+from google_auth_oauthlib import get_user_credentials
 from google.auth.transport.requests import AuthorizedSession
 from google.auth.transport.requests import Request
-from six.moves import http_client
 
-__all__ = ['acquire_credentials', 'set_global_volume']
+__all__ = ["acquire_credentials", "set_global_volume"]
 
 _BRAINMAPS_SCOPES = ["https://www.googleapis.com/auth/brainmaps"]
-_REFRESH_CODES = (http_client.UNAUTHORIZED, http_client.FORBIDDEN)
 
 
-def acquire_credentials(client_secret_file=None,
-                        client_id=None,
-                        client_secret=None,
-                        use_stored=True,
-                        store=True,
-                        make_global=True,
-                        gui_auth=False,
-                        storage_path=os.path.expanduser("~/creds.pickle")):
+def acquire_credentials(
+    client_secret_file=None,
+    client_id=None,
+    client_secret=None,
+    use_stored=True,
+    store=True,
+    make_global=True,
+    gui_auth=False,
+    storage_path=os.path.expanduser("~/brainmappy_creds.pickle"),
+):
     """Acquire credentials for brainmaps API and return a AuthorizedSession.
 
     client_secret_file or both client_id and client_secret are required on
@@ -47,33 +48,23 @@ def acquire_credentials(client_secret_file=None,
 
     if client_secret_file or (client_id and client_secret):
         if client_secret_file:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                client_secret_file,
-                scopes=_BRAINMAPS_SCOPES)
-        elif client_id and client_secret:
-            flow = InstalledAppFlow.from_client_config(
-                {
-                    "installed": {
-                        "client_id": client_id,
-                        "client_secret": client_secret,
-                        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-                        "token_uri": "https://oauth2.googleapis.com/token",
-                    }
-                },
-                scopes=_BRAINMAPS_SCOPES,
-                redirect_uri="urn:ietf:wg:oauth:2.0:oob")
-        else:
-            raise Exception("Valid credentials required.")
+            with open(client_secret_file, "r") as f:
+                secret = json.load(f)
+            client_id = secret["installed"]["client_id"]
+            client_secret = secret["installed"]["client_secret"]
+        elif not client_id or not client_secret:
+            raise Exception(
+                "Must either provide a `client_secret_file` or `client_id` and `client_secret`."
+            )
 
-        if gui_auth:
-            flow.run_local_server()
-        else:
-            flow.run_console()
-
-        session = flow.authorized_session()
-        creds = flow.credentials
+        creds = get_user_credentials(
+            scopes=_BRAINMAPS_SCOPES,
+            client_id=client_id,
+            client_secret=client_secret,
+        )
+        session = AuthorizedSession(creds)
     elif use_stored and os.path.isfile(storage_path):
-        with open(storage_path, 'rb') as token:
+        with open(storage_path, "rb") as token:
             creds = pickle.load(token)
         session = AuthorizedSession(creds)
     else:
@@ -83,16 +74,16 @@ def acquire_credentials(client_secret_file=None,
         if creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            raise Exception('Credentials invalid but unable to refresh.')
+            raise Exception("Credentials invalid but unable to refresh.")
 
     if store:
         # Save the credentials for the next run
-        with open(storage_path, 'wb') as token:
+        with open(storage_path, "wb") as token:
             pickle.dump(creds, token)
 
     if make_global:
-        sys.modules['brainmap_credentials'] = creds
-        sys.modules['brainmap_session'] = session
+        sys.modules["brainmap_credentials"] = creds
+        sys.modules["brainmap_session"] = session
 
     return session
 
@@ -100,16 +91,18 @@ def acquire_credentials(client_secret_file=None,
 def _eval_session(session=None, raise_error=True):
     """Evaluate brainmaps session and checks for globally defined session."""
     if session is None:
-        if 'brainmap_session' in sys.modules:
-            return sys.modules['brainmap_session']
-        elif 'brainmap_session' in globals():
-            return globals()['brainmap_session']
+        if "brainmap_session" in sys.modules:
+            return sys.modules["brainmap_session"]
+        elif "brainmap_session" in globals():
+            return globals()["brainmap_session"]
         else:
             if raise_error:
-                raise Exception('Please either pass a brainmaps session or '
-                                'define globally as "service" ')
+                raise Exception(
+                    "Please either pass a brainmaps session or "
+                    'define globally as "service" '
+                )
     elif not isinstance(session, AuthorizedSession):
-        error = 'Expected None or Resource, got {}'.format(type(session))
+        error = "Expected None or Resource, got {}".format(type(session))
         if raise_error:
             raise TypeError(error)
 
@@ -123,21 +116,24 @@ def _eval_session(session=None, raise_error=True):
 def _eval_volumeId(volumeId, raise_error=True):
     """Evaluate volume Id and checks for globally defined service."""
     if volumeId is None:
-        if 'volumeId' in sys.modules:
-            return sys.modules['volumeId']
-        elif 'volumeId' in globals():
-            return globals()['volumeId']
+        if "volumeId" in sys.modules:
+            return sys.modules["volumeId"]
+        elif "volumeId" in globals():
+            return globals()["volumeId"]
         else:
             if raise_error:
-                raise Exception('Please either pass a volumeId as string or '
-                                'define globally as "volumeId"')
+                raise Exception(
+                    "Please either pass a volumeId as string or "
+                    'define globally as "volumeId"'
+                )
     elif not isinstance(volumeId, str):
-        error = 'Expected None or string, got {}'.format(type(volumeId))
+        error = "Expected None or string, got {}".format(type(volumeId))
         if raise_error:
             raise TypeError(error)
 
     return volumeId
 
+
 def set_global_volume(vol):
     """Set global volume."""
-    sys.modules['volumeId'] = vol
+    sys.modules["volumeId"] = vol
